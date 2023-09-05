20e5adde372b33014a298d6f9f1695c59503706b
Vishakumar <89530242+thewebunity@users.noreply.github.com>
2023-09-05 09:47:07 +0000

    math

---
+++ basic_08/index.js
@@ -0,0 +1,26 @@
+// console.log(Math)
+
+// change number into positive 
+
+// console.log(Math.abs(-4))
+
+// console.log((Math.round(5.6)))
+
+// console.log(Math.ceil(5.6))
+
+// console.log(Math.floor(5.7))
+
+// console.log(Math.max(4,5,8,9,11))
+
+// console.log(Math.min(4,5,8,9,11))
+
+console.log(Math.random())
+
+console.log((Math.random() * 10) + 1);
+
+console.log((Math.random() * 10) + 1);
+
+const min = 10; 
+const max = 20 ;
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min)
