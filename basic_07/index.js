ff0e2cf7c40e97b2a77c2f19b1e0d21f37b603ee
Vishakumar <89530242+thewebunity@users.noreply.github.com>
2023-09-05 09:34:29 +0000

    number

---
+++ basic_07/index.js
@@ -0,0 +1,26 @@
+// initliaze number 
+
+const score  = 400 
+
+
+// console.log(score)
+
+
+// with the help of new keyword
+const balance = new Number(100)
+
+
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed('1'))
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4))
+
+
+const long_number = 100000000000
+
+// console.log(long_number.toLocaleString('en-IN'))
